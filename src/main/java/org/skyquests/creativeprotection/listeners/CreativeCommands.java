0e27fd18d9f17215c6128f59f307fceaeccb0e41
Corin22225 <mrcoltj@gmail.com>
2016-11-27 13:10:32 -0500

    Check if the player is in creative or not

--- src/main/java/org/skyquests/creativeprotection/listeners/CreativeCommands.java
+++ src/main/java/org/skyquests/creativeprotection/listeners/CreativeCommands.java
@@ -1,6 +1,7 @@
 package org.skyquests.creativeprotection.listeners;
 
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,8 +14,10 @@ public class CreativeCommands implements Listener {
         Player player = event.getPlayer();
 
         if (event.getMessage().contains("/sell") || event.getMessage().contains("/ah") || event.getMessage().contains("/echest") || event.getMessage().contains("/pv") || event.getMessage().contains("/eechest") || event.getMessage().contains("/enderchest") || event.getMessage().contains("/eenderchest")) {
-            player.sendMessage(ChatColor.RED + "You can not perform this command while in creative.");
-            event.setCancelled(true);
+            if (player.getGameMode() == GameMode.CREATIVE) {
+                player.sendMessage(ChatColor.RED + "You can not perform this command while in creative.");
+                event.setCancelled(true);
+            }
         }
     }
 }
